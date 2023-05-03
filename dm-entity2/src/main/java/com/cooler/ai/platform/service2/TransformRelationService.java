1ba9c65d6ac8be41b309c8cee02899cb5204ddb7
cooler130 <zzss_2003@163.com>
2023-05-04 00:47:29 +0800

    大的变动： 1.添加了DataVersion表，dm-entity中实体数据库中可以加多版本数据，distribution-entity不要用多版本。 2.dialog-bot-tool工具优化，可以根据neo4j图自动添加dm-entity中的数据，其第三步骤添加distribution-entity慎用。 3.bot-burouter2改为bot-servers，可以在此模块设置多个bot，只需要建立不同xml文件，指向不同bot的数据库。 4.添加了boss_hiring的bot，NLU已经添加了实例数据，测试案例写了两个。

--- dm-entity2/src/main/java/com/cooler/ai/platform/service2/TransformRelationService.java
+++
@@ -1,9 +0,0 @@
-package com.cooler.ai.platform.service2;
-
-import com.cooler.ai.platform.entity2.TransformRelation;
-
-public interface TransformRelationService {
-
-    TransformRelation selectTransformIntent(String domainName, String taskName, String contextState, String intentName);
-
-}
